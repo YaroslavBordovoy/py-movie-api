72d1312df8837afaae3ef6963b1c7d939c535b9f
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-12-08 10:28:46 +0200

    feat: add model and serializer

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=101)
+    description = models.TextField(null=True, blank=True)
+    duration = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.title} (description: {self.description}, duration: {self.duration})"
